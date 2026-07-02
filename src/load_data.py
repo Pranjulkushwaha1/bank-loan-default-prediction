41f0824619e7e6a6bb45f25b5163d92d15bc814f
Pranjul <kushwahapranjul062@gmail.com>
2026-07-02 07:56:02 +0530

    Switched to PostgreSQL, updated load_data.py and requirements.txt

--- src/load_data.py
+++ src/load_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+# PostgreSQL connection
+engine = create_engine("postgresql+psycopg2://postgres:154269@localhost/bank_loan_db")
+
 df = pd.read_csv("data/cs-training.csv")
 print(df.shape)
-
-engine = create_engine("mysql+pymysql://root:@localhost/bank_loan_db")
 
 df.to_sql("loan_data", con=engine, if_exists="replace", index=False)
 print("Data loaded successfully!")
