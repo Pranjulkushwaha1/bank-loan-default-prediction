d247ad64bf0dd14587a135089139dd48e3e6e1f7
Pranjul <kushwahapranjul062@gmail.com>
2026-08-16 01:29:37 +0530

    fix: add JWT auth headers to pytest tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,13 @@ from src.app import app
 
 client = TestClient(app)   # ye fast api app ko test mode me chalata hai
 
+def get_auth_token():
+    response = client.post("/auth/token", data={
+        "username": "user",
+        "password": "user123"
+    })
+    return response.json()["access_token"]
+
 def test_health():
     response = client.get("/v1/health")
     assert response.status_code == 200
@@ -10,6 +17,7 @@ def test_health():
 
 
 def test_predict_valid():
+    token = get_auth_token()
     response = client.post("/v1/predict", json={
         "RevolvingUtilizationOfUnsecuredLines": 0,
         "age": 0,
@@ -21,14 +29,18 @@ def test_predict_valid():
         "NumberRealEstateLoansOrLines": 0,
         "NumberOfTime60_89DaysPastDueNotWorse": 0,
         "NumberOfDependents": 0
-    })
+    },
+    headers={"Authorization": f"Bearer {token}"}
+    )
+    
     assert response.status_code == 200
     assert "prediction" in response.json()
     assert "probability" in response.json()
     assert "risk_category" in response.json()
 
 def test_predict_invalid():
-        response = client.post("/v1/predict", json={
-             "age":"abs"
-        })
-        assert response.status_code == 422
+    token = get_auth_token()          # ← ye add karo
+    response = client.post("/v1/predict", json={
+         "age":"abs"
+    }, headers={"Authorization": f"Bearer {token}"})  # ← ye add karo
+    assert response.status_code == 422
