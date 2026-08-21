849fd16a59e4fe9f8db85a04c38f0d1d0b27cd32
Pranjul <kushwahapranjul062@gmail.com>
2026-08-21 23:54:46 +0530

    Rag Updated

--- src/rag.py
+++ src/rag.py
@@ -4,9 +4,29 @@ from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import Chroma
 from sentence_transformers import SentenceTransformer
 import os
+from langchain.embeddings.base import Embeddings
+from typing import List
 
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 loader = TextLoader("data/loan_policy.txt")
 documents = loader.load()
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500, 
+    chunk_overlap=50
+)
+chunks = text_splitter.split_documents(documents)
+
+class SentenceTransformerEmbeddings(Embeddings):
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return embedding_model.encode(texts).tolist()
+    
+    def embed_query(self, text: str) -> List[float]:
+        return embedding_model.encode(text).tolist()
+
+embeddings = SentenceTransformerEmbeddings()
+vectorstore = Chroma.from_documents(chunks, embeddings)
+retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+    
+ask_rag()
