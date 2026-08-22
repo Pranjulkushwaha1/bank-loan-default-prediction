22b710332877b3a25cf09f3487ee291443225fd9
Pranjul <kushwahapranjul062@gmail.com>
2026-08-22 23:17:19 +0530

    feat: Add RAG pipeline with ChromaDB and /v1/ask endpoint

--- src/rag.py
+++ src/rag.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.prompts import PromptTemplate
+from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores import Chroma
 from sentence_transformers import SentenceTransformer
 import os
